9c8b6ad0dc335dd859721d75125f45e258de18da
pawbu <pawbu@users.noreply.github.com>
2023-12-09 13:12:26 +0100

    Day 6: 2/2

--- src/day06/solution.ts
+++ src/day06/solution.ts
@@ -18,24 +18,36 @@ const distance = input[1]
 
 const part1 = times
   .map((time, i) => {
-    let timeRange = Array.from(
-      { length: time - 1 },
-      (_value, index) => index + 1
-    );
-    let currentTime = time;
-    let currentDistance = distance[i];
-    let canWin = timeRange
-      .map((time) => {
-        let timeLeft = currentTime - time;
-        let speed = time;
-        let distance = timeLeft * speed;
-        if (distance > currentDistance) {
-          return true;
-        } else return false;
-      })
-      .filter((value) => value === true).length;
-    return canWin;
+    return numberOfWins(time, distance[i]);
   })
   .reduce((a, b) => a * b, 1);
 
 console.log("Part 1: " + part1);
+
+function numberOfWins(time: number, distance: number) {
+  let timeRange = Array.from(
+    { length: time - 1 },
+    (_value, index) => index + 1
+  );
+  let currentTime = time;
+  let currentDistance = distance;
+  let canWin = timeRange
+    .map((time) => {
+      let timeLeft = currentTime - time;
+      let speed = time;
+      let distance = timeLeft * speed;
+      if (distance > currentDistance) {
+        return true;
+      } else return false;
+    })
+    .filter((value) => value === true).length;
+  return canWin;
+}
+
+const timesPart2 = parseInt(input[0].replace("Time: ", "").replaceAll(" ", ""));
+
+const distancePart2 = parseInt(
+  input[1].replace("Distance: ", "").replaceAll(" ", "")
+);
+
+console.log("Part 2: " + numberOfWins(timesPart2, distancePart2));
